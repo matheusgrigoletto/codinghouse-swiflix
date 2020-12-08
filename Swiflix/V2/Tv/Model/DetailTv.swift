59be8f207ed8ebd667ee0f69b610a9fdab1a2c9d
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2020-12-08 15:46:44 -0300

    Implementado modelo de Series conforme documentação da API

--- Swiflix/V2/Tv/Model/DetailTv.swift
+++ Swiflix/V2/Tv/Model/DetailTv.swift
@@ -8,20 +8,37 @@
 import Foundation
 
 struct DetailTv {
-    let name: String
-    let original_name: String
-    let poster_path:String
-    let id: Int
-    let backdrop_path:String
-    let vote_average:Double
-    let overview: String
+    
+    let backdrop_path: String?
+    let created_by: [Any]
     let episode_run_time: [Int]
     let first_air_date: String
-    let genres: [Genre]
-    let in_production:Bool
+    let genres: [Any]
+    let homepage: String
+    let id: Int
+    let in_production: Bool
+    let languages: [String]
+    let last_air_date: String
+    let last_episode_to_air: Any
+    let name: String
+    let next_episode_to_air: Any?
+    let networks: [Any]
     let number_of_episodes: Int
     let number_of_seasons: Int
-    let seasons: [Season]
+    let origin_country: [String]
+    let original_language: String
+    let original_name: String
+    let overview: String
+    let popularity: Double
+    let poster_path: String?
+    let production_companies: [Any]
+    let production_countries: [Any]
+    let seasons: [Any]
+    let spoken_languages: [Any]
     let status: String
+    let tagline: String
     let type: String
+    let vote_average: Double
+    let vote_count: Int
+    
 }
