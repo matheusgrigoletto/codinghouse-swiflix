4d29461b551edb408c8dd901ca5cd41640c583cf
Bruno Horta <brhorta@gmail.com>
2021-01-22 00:46:17 -0300

    Campos de Cadastro mais visiveis - Falta validacao

--- Swiflix/Controller/Initial/RegisterViewController.swift
+++ Swiflix/Controller/Initial/RegisterViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Firebase
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, UITextFieldDelegate {
 
     //MARK: - IBOutlets
     @IBOutlet weak var nomeTextField: UITextField!
@@ -20,11 +20,74 @@ class RegisterViewController: UIViewController {
     
     //MARK: - View LifeCycle
     override func viewDidLoad() {
+        
+        nomeTextField.delegate = self
+        emailTextField.delegate = self
+        confirmarEmailTextField.delegate = self
+        senhaTextField.delegate = self
+        confirmarSenhaTextField.delegate = self
+        
+        
+        
         super.viewDidLoad()
         escondeTecladoClicandoFora()
+        
+        nomeTextField.backgroundColor = .systemGray
+        emailTextField.backgroundColor = .systemGray
+        confirmarEmailTextField.backgroundColor = .systemGray
+        senhaTextField.backgroundColor = .systemGray
+        confirmarSenhaTextField.backgroundColor = .systemGray
+        
+        
     }
     
+  
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == self.nomeTextField {
+            self.nomeTextField.backgroundColor = .white
+            self.nomeTextField.textColor = .black
+        } else if textField == self.emailTextField {
+            self.emailTextField.backgroundColor = .white
+            self.emailTextField.textColor = .black
+        } else if textField == self.confirmarEmailTextField {
+            self.confirmarEmailTextField.backgroundColor = .white
+            self.confirmarEmailTextField.textColor = .black
+        } else if textField == self.senhaTextField {
+            self.senhaTextField.backgroundColor = .white
+            self.senhaTextField.textColor = .black
+        } else if textField == self.confirmarSenhaTextField {
+            self.confirmarSenhaTextField.backgroundColor = .white
+            self.confirmarSenhaTextField.textColor = .black
+        }
+    }
+    
+    
+//MARK: - Tentativa de validar email
+    //Validar TextFields emails iguais e senhas iguais
+//    func textFieldDidEndEditing(_ textField: UITextField) {
+//        if textField == self.confirmarEmailTextField{}
+//            if self.confirmarEmailTextField != self.emailTextField{
+//                alertaEmail()
+//            }
+//        }
+//
+
+   
+    
+//     func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+//        textField.backgroundColor = .systemGray
+//
+//        if confirmarEmailTextField.isSelected == true {
+//            if self.confirmarEmailTextField != self.emailTextField{
+//                alertaEmail()
+//            }
+//        }
+    
+//     }
+    
+    
     //MARK: - IBActions
+    
     @IBAction func cadastrarButtonTapped(_ sender: UIButton) {
          //TODO: Implementar função de criar novo usuário aqui
         
@@ -32,5 +95,21 @@ class RegisterViewController: UIViewController {
     @IBAction func chooseImageButtonTapped(_ sender: UIButton) {
     }
     
+    
     //MARK: - Functions
+    
+    
+    func alertaEmail(){
+        let refreshAlert = UIAlertController(title: "Atenção", message: "Os e-mails não conferem!", preferredStyle: UIAlertController.Style.alert)
+
+        refreshAlert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction!) in
+              //print("Handle Ok logic here")
+        }))
+
+//        refreshAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action: UIAlertAction!) in
+//              print("Handle Cancel Logic here")
+//        }))
+
+        present(refreshAlert, animated: true, completion: nil)
+    }
 }
