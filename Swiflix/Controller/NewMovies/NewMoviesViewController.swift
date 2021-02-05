197c428aec71972da676509f9630f7882ef8fddc
Vanessa Montagner <vanessamontagner@hotmail.com>
2021-02-05 11:09:06 -0300

    Adicionado Search na tela Lançamentos

--- Swiflix/Controller/NewMovies/NewMoviesViewController.swift
+++ Swiflix/Controller/NewMovies/NewMoviesViewController.swift
@@ -14,8 +14,14 @@ class NewMoviesViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     
     var movies:[GenericMedia] = []
+    let date = Date()
+    let calendar = Calendar.current
+    
     
     override func viewDidLoad() {
+        
+        searchBar.placeholder = "Procure por um filme"
+        
         super.viewDidLoad()
         escondeTecladoClicandoFora()
         self.registerCell(nib: GenericMediaTableViewCell.nibName, cellID: GenericMediaTableViewCell.cellID)
@@ -25,8 +31,45 @@ class NewMoviesViewController: UIViewController {
         
     }
     
-    private func getNewMovies(){
+    
+    
+    private func getSearchNewMovies(searchText: String){
+        self.movies = []
+        let ano = calendar.component(.year, from: date)
         
+
+        SearchMDB.movie(query: searchText, language: "pt-BR", page: 1, includeAdult: false, year: ano, primaryReleaseYear: ano) { (return, movies) in
+
+
+            if let movies = movies {
+
+                for movie in movies {
+
+                    if
+
+                        let title = movie.title,
+                        let id = movie.id,
+                        let rating = movie.vote_average,
+                        let overview = movie.overview,
+                        let poster = movie.poster_path{
+                        let generic = GenericMedia(id: id, title: title, rating: rating, overview: overview, poster: poster)
+
+                        self.movies.append(generic)
+
+
+                    }
+                }
+                self.newMoviesTableView.reloadData()
+
+            }
+        }
+
+    }
+    
+    
+    
+    
+    private func getNewMovies(){
         
         MovieMDB.upcoming(page: 1, language: "pt-BR") { (return, movies) in
             if let movies = movies {
@@ -57,6 +100,7 @@ class NewMoviesViewController: UIViewController {
     func configureDelegates(){
         self.newMoviesTableView.delegate = self
         self.newMoviesTableView.dataSource = self
+        self.searchBar.delegate = self
     }
     
         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -70,6 +114,11 @@ class NewMoviesViewController: UIViewController {
 }
 
     extension NewMoviesViewController: UITableViewDataSource {
+        
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+            cell.contentView.layer.masksToBounds = true
+    }
+        
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.movies.count
     }
@@ -93,6 +142,42 @@ extension NewMoviesViewController: UITableViewDelegate {
         performSegue(withIdentifier: Segues.toNewMovieDetail, sender: chosenMovie)
     }
 }
+
+
+extension NewMoviesViewController: UISearchBarDelegate{
     
+    
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar){
+        searchBar.resignFirstResponder()
+        
+        
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        
+     
+
+    }
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+       
+    }
+    
+    
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
+        if (!searchText.isEmpty){
+            getSearchNewMovies(searchText: searchText)
+        }else{
+            self.movies = []
+            getNewMovies()
+        }
+        
+    }
+    
+}
+    
+
+
 
 
